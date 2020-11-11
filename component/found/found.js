57ef564c2cb15e49122f89650357db978abe25c9
ttt2021 <570308307@qq.com>
2020-11-11 15:25:29 +0800

    watch

--- component/found/found.js
+++ component/found/found.js
@@ -21,36 +21,7 @@ Component({
      * 组件的方法列表
      */
     methods: {
-        changeColor: function (e) {
-            var that = this 
-            let arr = that.data.allFound
-            arr.forEach((item, index) => {
-              // console.log(item.sign)
-              if (item.sign == '市场') {
-                let temp = "allFound[" + index + "].signIcon"
-                this.setData({
-                  [temp]: 'background-color: #ff8800'
-                })
-              }
-              if (item.sign == '百科') {
-                let temp = "allFound[" + index + "].signIcon"
-                this.setData({
-                  [temp]: 'background-color: #0066ff'
-                })
-              }
-              if (item.sign == '政策') {
-                let temp = "allFound[" + index + "].signIcon"
-                this.setData({
-                  [temp]: 'background-color: #66cc33'
-                })
-              }
-              if (item.sign == '') {
-                let temp = "allFound[" + index + "].signIcon"
-                this.setData({
-                  [temp]: 'display: none'
-                })
-              }
-            })
-        }
+        
     }
+
 })
